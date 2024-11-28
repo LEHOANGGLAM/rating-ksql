e6053ec53b3983f3e9b88f38bfe31eb15f33873c
lamlecamhoang <lam.lecamhoang@unified.com>
2024-11-28 09:54:42 +0700

    update

--- scripts/create-mysql-source.sh
+++ scripts/create-mysql-source.sh
@@ -11,7 +11,7 @@ curl -i -X POST -H "Accept:application/json" \
             "database.user": "debezium",
             "database.password": "dbz",
             "database.server.id": "42",
-            "database.server.name": "asgard",
+            "database.server.name": "server_name",
             "table.whitelist": "demo.customers",
             "database.history.kafka.bootstrap.servers": "kafka:29092",
             "database.history.kafka.topic": "dbhistory.demo" ,
@@ -25,30 +25,6 @@ curl -i -X POST -H "Accept:application/json" \
             "transforms.InsertTopic.topic.field":"messagetopic",
             "transforms.InsertSourceDetails.type":"org.apache.kafka.connect.transforms.InsertField$Value",
             "transforms.InsertSourceDetails.static.field":"messagesource",
-            "transforms.InsertSourceDetails.static.value":"Debezium CDC from MySQL on asgard"
+            "transforms.InsertSourceDetails.static.value":"Debezium CDC from MySQL on server_name"
        }
-    }'
-curl -i -X POST -H "Accept:application/json" \
-    -H  "Content-Type:application/json" http://connect-debezium:8083/connectors/ \
-    -d '{
-      "name": "mysql-source-demo-customers-raw",
-      "config": {
-            "connector.class": "io.debezium.connector.mysql.MySqlConnector",
-            "database.hostname": "mysql",
-            "database.port": "3306",
-            "database.user": "debezium",
-            "database.password": "dbz",
-            "database.server.id": "43",
-            "database.server.name": "asgard",
-            "table.whitelist": "demo.customers",
-            "database.history.kafka.bootstrap.servers": "kafka:29092",
-            "database.history.kafka.topic": "dbhistory.demo-raw" ,
-            "include.schema.changes": "true",
-            "key.converter":"org.apache.kafka.connect.storage.StringConverter",
-            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-            "value.converter.schemas.enable": false,
-            "transforms": "addTopicSuffix",
-            "transforms.addTopicSuffix.type":"org.apache.kafka.connect.transforms.RegexRouter",
-            "transforms.addTopicSuffix.regex":"(.*)",
-            "transforms.addTopicSuffix.replacement":"$1-raw"       }
     }'
